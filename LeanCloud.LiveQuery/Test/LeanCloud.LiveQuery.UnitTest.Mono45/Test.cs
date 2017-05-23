a6ade124aa9eb692af7a73a4bcf0eb50102e18a2
WuJun <wujun19890209@gmail.com>
2017-05-23 17:12:48 +0800

    chore: upgrade dependency.

--- LeanCloud.LiveQuery/Test/LeanCloud.LiveQuery.UnitTest.Mono45/Test.cs
+++ LeanCloud.LiveQuery/Test/LeanCloud.LiveQuery.UnitTest.Mono45/Test.cs
@@ -44,6 +44,26 @@ namespace LeanCloud.LiveQuery.UnitTest.Mono45
                 {
                     doingList.Add(e.Payload);
                 }
+				if (e.Scope == "enter")
+				{
+					doingList.Add(e.Payload);
+				}
+                if(e.Scope == "leave")
+                {
+                    var done = doingList.First(todo => todo.ObjectId == e.Payload.ObjectId);
+                    if(done != null)
+                    {
+                        doingList.Remove(done);
+                    }
+                }
+				if (e.Scope == "delete")
+				{
+					var done = doingList.First(todo => todo.ObjectId == e.Payload.ObjectId);
+					if (done != null)
+					{
+						doingList.Remove(done);
+					}
+				}
             };
 
 			var testObj = new AVObject("Todo");
@@ -53,6 +73,29 @@ namespace LeanCloud.LiveQuery.UnitTest.Mono45
             var oneDoing = AVObject.CreateWithoutData("Todo", "5915bb92a22b9d005804a4ee");
             oneDoing["state"] = "done";
             await oneDoing.SaveAsync();
+
+            var anotherDone = AVObject.CreateWithoutData("Todo", "591672df2f301e006b9b2829");
+            anotherDone["state"] = "doing";
+			await anotherDone.SaveAsync();
+
+            var willDone = AVObject.CreateWithoutData("Todo", "591672df2f301e006b9b2829");
+            willDone["state"] = "done";
+            await willDone.SaveAsync();
+
+            var willDelete = AVObject.CreateWithoutData("Todo", "591d9b302f301e006be22c83");
+            await willDelete.DeleteAsync();
+
+            await AVUser.LogInAsync("tom","pwd123456");
+
+            var userQuery = new AVQuery<AVUser>();
+            var userLiveQuery = await userQuery.SubscribeAsync();
+            userLiveQuery.OnLiveQueryReceived += (sender, e) => 
+            {
+                if(e.Scope == "login")
+                {
+                    var user = e.Payload as AVUser;
+                }
+            };
 		}
         public List<AVObject> doingList = new List<AVObject>();
 		private void LivequeryInstance_OnLiveQueryReceived(object sender, AVLiveQueryEventArgs<AVObject> e)
@@ -62,8 +105,20 @@ namespace LeanCloud.LiveQuery.UnitTest.Mono45
 
             if(e.Scope == "update")
             {
-
+                foreach(var key in e.Keys)
+                {
+                    // 变更的列名
+                    if(key == "title")
+                    {
+                        // 获取对应的标题
+                        var title = e.Payload.Get<string>(key);
+                    }
+                }
             }
+			if (e.Scope == "enter")
+			{
+                doingList.Add(e.Payload);
+			}
 		}
 
     }
